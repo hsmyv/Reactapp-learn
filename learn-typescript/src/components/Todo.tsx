0857adf1eb82ddd85e8bfabcc2003ccc22094692
Hasan <hsmusayev@gmail.com>
2024-09-11 17:49:04 +0400

    added create todo list and display

--- learn-typescript/src/components/Todo.tsx
+++ learn-typescript/src/components/Todo.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 import {IoMdRemoveCircleOutline} from "react-icons/io";
 import {FaCheck} from "react-icons/fa";
 import { FaRegEdit } from 'react-icons/fa';
+import { TodoType } from '../types/Types';
 
-const Todo = () => {
+interface TodoProps{
+   todoProps: TodoType
+}
+const Todo = ({todoProps} :TodoProps) => {
+  const {id, content} = todoProps;
+
   return (
     <div className='todo'>
-        <div>        I am first Todo        </div>
+        <div>    {content}     </div>
     
         <div>
             <IoMdRemoveCircleOutline className='icons' style={{marginRight: '8px'}}/>
--- learn-typescript/src/components/TodoCreate.tsx
+++ learn-typescript/src/components/TodoCreate.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { createTodo } from '../redux/todoSlice';
+import { TodoType } from '../types/Types';
 
 const TodoCreate = () => {
+  const dispatch = useDispatch();
+
+  const [newTodo, setNewTodo] = useState<string>('');
+
+  const handleCreateTodo = () => {
+      if(newTodo.trim().length == 0){
+        alert("Todo enter!")
+        return;
+      }
+
+      const payload :TodoType = {
+        id: Math.floor(Math.random() * 999999),
+        content: newTodo
+      }
+
+      dispatch(createTodo(payload))
+      setNewTodo('');
+  }
   return (
     <div className='todo-create'>
-        <input placeholder='Todo enter...' className='todo-input' type="text" />
-        <button className='todo-create-button'>Create</button>
+        <input 
+        value={newTodo} 
+        onChange={(e:React.ChangeEvent<HTMLInputElement>) => setNewTodo(e.target.value)} 
+        placeholder='Todo enter...' className='todo-input' type="text" />
+        <button onClick={handleCreateTodo} className='todo-create-button'>Create</button>
     </div>
   )
 }
